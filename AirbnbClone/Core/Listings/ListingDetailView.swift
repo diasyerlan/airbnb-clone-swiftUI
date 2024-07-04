61ff41655ef07c2262b97e79fc828826423c7191
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-07-05 03:00:56 +0500

    UI finished

--- AirbnbClone/Core/Listings/ListingDetailView.swift
+++ AirbnbClone/Core/Listings/ListingDetailView.swift
@@ -27,7 +27,8 @@ struct ListingDetailView: View {
                                 .fill(.white)
                                 .frame(width: 32, height: 32)
                         }
-                        .padding(32)
+                        .padding(.horizontal, 32)
+                        .padding(.vertical, 48)
                 }
             }
             
@@ -157,6 +158,7 @@ struct ListingDetailView: View {
             }
             .padding()
         }
+        .toolbar(.hidden, for: .tabBar)
         .ignoresSafeArea()
         .padding(.bottom, 64)
         .overlay(alignment: .bottom) {
