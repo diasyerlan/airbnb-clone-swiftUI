9e320340640022d106ac11994d4f2c75bb4ef73a
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-07-07 14:02:51 +0500

    Map functionality added

---
+++ AirbnbClone/Extensions/Regions.swift
@@ -0,0 +1,14 @@
+//
+//  Regions.swift
+//  AirbnbClone
+//
+//  Created by Dias Yerlan on 07.07.2024.
+//
+
+import Foundation
+import CoreLocation
+
+extension CLLocationCoordinate2D {
+    static var LA = CLLocationCoordinate2D(latitude: 34.0549, longitude: -118.2426)
+    static var miami = CLLocationCoordinate2D(latitude: 25.7602, longitude: -80.1959)
+}
